4cde43595e1461a31ae567e8583a8280ea969bae
SuperSaiyanSSS <2304582602@qq.com>
2017-05-06 20:56:30 +0800

    GradientBoostingClassifier

--- ml/random_forest.py
+++ ml/random_forest.py
@@ -6,49 +6,69 @@ import pandas as pd
 import json
 import math
 import csv
+import pymongo
+import sklearn
 import sys
 sys.path.append("..")
+from a1 import base
+from a1 import sina_store
 reload(sys)
 sys.setdefaultencoding('utf-8')
-import datetime
-import difflib
-from wbcls import base
-from wbcls import sina_store
-from bs4 import BeautifulSoup
-import requests
-import time as tt
-import pymongo
-import re
-import sklearn
 
 
 class MachineLearning(base.SinaBaseObject):
-    def __init__(self, feature_vector_dict, is_first=False):
-        self.feature_vector_dict = feature_vector_dict
-        # 如果是第一次写csv则设置csv的头
-        self.is_first = is_first
-        self.clean_feture_vector_dict()
+    def __init__(self):
+        self.is_First = True
+        self.is_First_2 = True
+        self.gbc = ''
+        self.dtc = ''
+        self.rfc = ''
+
+    def set_feature_vector_dict(self, feature_vector_dict):
+        self.clean_feture_vector_dict(feature_vector_dict, is_first=self.is_First)
+        self.is_First = False
+
+    def set_test_feature_vector_dict(self, feature_vector_dict):
+        self.clean_test_feture_vector_dict(feature_vector_dict, is_first_2=self.is_First_2)
+        self.is_First_2 = False
 
     # 将传入的字典转化为csv文件
-    def clean_feture_vector_dict(self):
+    @staticmethod
+    def clean_feture_vector_dict(feature_vector_dict, is_first=False):
         with open('names.csv', 'ab') as csvfile:
             fieldnames = ['uid', 'similarity', 'platform', 'reputation', 'entropy', 'human_or_machine']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            if self.is_first:
+            if is_first:
                 writer.writeheader()
             writer.writerow(
-                {'uid': self.feature_vector_dict['uid'],
-                 'similarity': self.feature_vector_dict['similarity'],
-                 'platform': self.feature_vector_dict['platform'],
-                 'reputation': self.feature_vector_dict['reputation'],
-                 'entropy': self.feature_vector_dict['entropy'],
-                 'human_or_machine': self.feature_vector_dict['human_or_machine']
+                {'uid': feature_vector_dict['uid'],
+                 'similarity': feature_vector_dict['similarity'],
+                 'platform': feature_vector_dict['platform'],
+                 'reputation': feature_vector_dict['reputation'],
+                 'entropy': feature_vector_dict['entropy'],
+                 'human_or_machine': feature_vector_dict['human_or_machine']
+                 }
+            )
+
+    @staticmethod
+    def clean_test_feture_vector_dict(feature_vector_dict, is_first_2=False):
+        with open('needs.csv', 'ab') as csvfile:
+            fieldnames = ['uid', 'similarity', 'platform', 'reputation', 'entropy', 'human_or_machine']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            if is_first:
+                writer.writeheader()
+            writer.writerow(
+                {'uid': feature_vector_dict['uid'],
+                 'similarity': feature_vector_dict['similarity'],
+                 'platform': feature_vector_dict['platform'],
+                 'reputation': feature_vector_dict['reputation'],
+                 'entropy': feature_vector_dict['entropy'],
+                 'human_or_machine': feature_vector_dict['human_or_machine']
                  }
             )
 
     # 进行单一决策树和随机森林的训练模型及检验
-    @staticmethod
-    def rand_forest_train():
+    def rand_forest_train(self):
         # 读取本地用户特征信息
         users = pd.read_csv('names.csv')
         # 选取similarity、platform、reputation、entropy作为判别人类或机器的特征
@@ -77,6 +97,12 @@ class MachineLearning(base.SinaBaseObject):
         rfc.fit(X_train, y_train)
         rfc_y_pred = rfc.predict(X_test)
 
+        # 使用梯度提升决策树进行集成模型的训练及预测分析
+        from sklearn.ensemble import GradientBoostingClassifier
+        gbc = GradientBoostingClassifier()
+        gbc.fit(X_train, y_train)
+        gbc_y_pred = gbc.predict(X_test)
+
         from sklearn.metrics import classification_report
         # 输出单一决策树在测试集上的分类准确性， 以及更加详细的精确率 召回率 F1指标
         print("单一决策树的准确性为", dtc.score(X_test, y_test))
@@ -86,14 +112,48 @@ class MachineLearning(base.SinaBaseObject):
         print("随机森林分类器的准确性为", rfc.score(X_test, y_test))
         print(classification_report(rfc_y_pred, y_test))
 
+        # 输出梯度提升决策树在测试集上的分类准确性，以及更加详细的精确率 召回率 F1指标
+        print("梯度提升决策树的准确性为", gbc.score(X_test, y_test))
+        print(classification_report(gbc_y_pred, y_test))
 
-if __name__ == "__main__":
+
+        users = pd.read_csv('values.csv')
+
+        # 检验是否为机器或人类
+        X = users[['similarity', 'platform', 'reputation', 'entropy']]
+        X = vec.transform(X.to_dict(orient='record'))
+        print(rfc.predict(X))
+
+        self.dtc = dtc
+        self.rfc = rfc
+        self.gbc = gbc
+
+
+def get_dict_from_weibo_table():
+    ml = MachineLearning()
     sina_store_object = sina_store.SinaStore()
     sina_store_object.weibo_table = sina_store_object.db['human_vector_info']
     iter = sina_store_object.get_stored_information()
-    info_dict = next(iter)
-    print(info_dict)
-    print(info_dict["uid"])
-    a = MachineLearning(info_dict)
-    # info_dict = json.loads(info_dict)
-    # dataCopy = pd.read_json(info_dict, typ='frame')
+    while True:
+        try:
+            info_dict = next(iter)
+            ml.set_feature_vector_dict(info_dict)
+        except StopIteration:
+            break
+    sina_store_object.weibo_table = sina_store_object.db['machine_vector_info']
+    iter = sina_store_object.get_stored_information()
+    while True:
+        try:
+            info_dict = next(iter)
+            ml.set_feature_vector_dict(info_dict)
+        except StopIteration:
+            break
+    print("已结束 正在训练模型。。。")
+    ml.rand_forest_train()
+
+
+def start_training():
+    get_dict_from_weibo_table()
+
+if __name__ == "__main__":
+    start_training()
