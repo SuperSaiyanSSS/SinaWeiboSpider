b75793f121c9e47a1cdcbb41d64a1c7dcdef0670
SuperSaiyanSSS <2304582602@qq.com>
2017-08-11 22:27:03 +0800

    test

--- test.py
+++ test.py
@@ -7,7 +7,6 @@ sys.setdefaultencoding('utf-8')
 cookies = \
 'ALF=1504271525; SCF=AjsEaVa0e8KjEg3yEjwEx270PLOpYvK-1BhV7AdkMSQgREMFA0bd3IqV3weK9ydf5NAbDXjKT69Rfndb2m9Ah4I.; SUB=_2A250hb_2DeRhGeNH7VIV9izNwj2IHXVXicG-rDV6PUNbktANLRPhkW1ZeSLr49kFNMgwrWThnh1bPUhWPw..; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhaydrjX2CLPhFdjQ77gn4P5JpX5KMhUgL.Fo-4So5XSozp1K22dJLoI0YLxK.LB.-L1K.LxKML12qLBK5LxKqL1KBLBo.LxK.LB-BL1KBLxKBLB.2LB.2LxK-LBonL1heLxKqLB-eLBKMt; SUHB=03oF_eQuesy4xQ; SSOLoginState=1501679526; _T_WM=544d051d212d2d6f3adece8b6949b373'
 if __name__ == '__main__':
-
     pe0 = WeiboClient(cookies=cookies)
     pe2 = pe0.Weibo('E6iRJofK6')
     pe4 = pe0.People('1884866222')
